2703d05674d2d5230a94cfcdfd25e5497f3a2583
DarleneRobot <mikeburrowed@gmail.com>
2017-01-30 05:48:44 +1300

    Update csgoProfileSkim.py

--- Python/CSGO/csgoProfileSkim.py
+++ Python/CSGO/csgoProfileSkim.py
@@ -3,6 +3,7 @@ from ctypes import wintypes
 import time, pyperclip
 from random import shuffle
 import webbrowser, sys, pyperclip, requests, bs4, re, getopt
+from prettytable import PrettyTable
 name = 'User'
 
 
@@ -152,5 +153,116 @@ def profileInfo():
 
 	except KeyboardInterrupt:
 	    print('done')
+	
+
+def lobbied():
+	urlArray = []
+
+	def copy(two):
+	    PressKey(0x09)
+	    time.sleep(0.3)
+	    pyautogui.click(629, two, button='right')
+	    pyautogui.click(629, two)
+	    pyautogui.click(710, two + 38)  # click steam profile
+	    time.sleep(2.5)
+	    ReleaseKey(0x09)
+	    pyautogui.click(637, 231, button='right')  # right click
+	    pyautogui.click(741, 356)  # copy url
+	    time.sleep(0.5)
+	    urlArray.append(pyperclip.paste())
+	    pyautogui.click(1907, 16)  # exit page
+	    pyautogui.press('esc')  # escape
+	    time.sleep(0.5)
 
 
+	def top():
+	    copy(367)
+	    copy(407)
+	    copy(440)
+	    copy(470)
+	    copy(500)
+
+	def bottom():
+	    copy(619)
+	    copy(657)
+	    copy(695)
+	    copy(725)
+	    copy(755)
+
+	if pyautogui.prompt('top or bottom team?') == 'top':
+	    top()
+	else:
+	    bottom()
+
+
+	try:
+
+	    default = 0.1
+	    #name = pyperclip.paste()
+	    steamURL = pyperclip.paste()
+
+	    nameArray = []
+
+	    for x in range(len(urlArray)):
+		res = requests.get(urlArray[x])
+		res.raise_for_status()
+		userProfile = bs4.BeautifulSoup(res.text, "html.parser")
+		user = userProfile.select('.actual_persona_name')
+		name = user[0].getText().strip()
+		nameArray.append(name)
+
+	    for z in range(len(urlArray)):
+		res = requests.get(urlArray[z])
+		res.raise_for_status()
+		userProfile = bs4.BeautifulSoup(res.text, "html.parser")
+		friends = ''
+
+		resFriends = requests.get(urlArray[z] + '/friends')
+		userFriends = bs4.BeautifulSoup(resFriends.text, "html.parser")
+		usersName = userFriends.select('.whiteLink')
+		elemsFriends = userFriends.select('.friendBlockContent')
+
+		user = userProfile.select('.actual_persona_name')
+		name = user[0].getText().strip()
+
+		hours = userProfile.select('.game_info_details')
+
+		try:
+		    table = PrettyTable([name,re.sub("Currently In-Game.*", "", hours[0].getText()).strip()])
+		except:
+		    print(name + '  private profile')
+
+
+		try:
+		    for j in range(len(elemsFriends)):
+			for k in range(len(nameArray)):
+			    if ((re.sub("\n.*", "", elemsFriends[j].getText().strip())) == nameArray[k]):
+				table.add_row(['friend', re.sub("\n.*", "", elemsFriends[j].getText().strip())])
+				friends += (re.sub("\n.*", "", elemsFriends[j].getText().strip()))
+				friends += '   '
+
+		except:
+		    print('private')
+
+		try:
+		    print (table)
+		    c(name + ' friends in game: ' + friends)
+		except:
+		    print('cant print table')
+
+
+
+		print()
+
+
+	except:
+	    print ('error')
+
+script = pyautogui.prompt('enter: profile, lobbied, or fake')
+
+if script == 'profile':
+	profileInfo()
+else if script == 'lobbied':
+	lobbied()
+else if script == 'fake':
+	
